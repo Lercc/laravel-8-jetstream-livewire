837dcfe4c0489dfa913081e9ce714dbbdef0d922
lercc <lercc.en@gmail.com>
2021-04-06 19:45:07 -0500

    make index layout

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+    
